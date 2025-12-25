894152b9cf0ceec63b83ce787dfd8617bf4dcdc4
AlbertoWebDev <albertovazquezmonge@gmail.com>
2025-12-25 18:47:56 +0100

    Changed Backend to spring project

---
+++ src/main/java/com/tfg/Vpad/VpadApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.Vpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/Vpad/VpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.Vpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

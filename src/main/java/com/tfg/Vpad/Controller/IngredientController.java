c839b080a614cdbff2e3238606e751756a3a0775
AlbertoWebDev <albertovazquezmonge@gmail.com>
2025-12-26 21:04:06 +0100

    Endpoint called via axios API in react

--- src/main/java/com/tfg/Vpad/Controller/IngredientController.java
+++ src/main/java/com/tfg/Vpad/Controller/IngredientController.java
@@ -3,15 +3,16 @@ package com.tfg.Vpad.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tfg.Vpad.Entity.Ingredient;
 import com.tfg.Vpad.Service.IngredientService;
-
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") // Made to handle CORS issues with Vite server
 public class IngredientController {
     
     private final IngredientService ingredientService;

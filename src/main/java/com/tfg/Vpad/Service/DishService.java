6aaaaf1d671091d99392ed6586b3e1eeca5f8c4f
AlbertoWebDev <albertovazquezmonge@gmail.com>
2026-01-28 11:53:48 +0100

    Functions at DishService created

--- src/main/java/com/tfg/Vpad/Service/DishService.java
+++ src/main/java/com/tfg/Vpad/Service/DishService.java
@@ -21,4 +21,22 @@ public class DishService {
     public List<Dish> getAllDishes() {
         return dishRepository.findAll();
     }
+
+    public Dish saveDish(Dish dish) {
+    return dishRepository.save(dish);
+    }
+
+    public Dish updateDish(Long id, Dish dish) {
+        Dish existingDish = dishRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Dish con el id: " + id + " no encontrado"));
+        existingDish.setName(dish.getName());
+        existingDish.setDescription(dish.getDescription());
+        existingDish.setPrice(dish.getPrice());
+        existingDish.setIngredients(dish.getIngredients());
+        return dishRepository.save(existingDish);
+    }
+
+    public void deleteDish(Long id) {
+        dishRepository.deleteById(id);
+    }
 }
